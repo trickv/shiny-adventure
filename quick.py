641ad4d6aa7d822ccf8e4a4ae714e8e4baa1d136
Patrick van Staveren <trick@vanstaveren.us>
2023-01-30 23:57:45 +0000

    connect repeatedly, and then poll data

--- quick.py
+++ quick.py
@@ -1,28 +1,45 @@
 #!/usr/bin/env python3
 
+import time
+import sys
 import obd
 
-connection = obd.OBD("/dev/rfcomm0", fast=False)
+for x in range(1, 200):
+    print("try {}...".format(x))
+    connection = obd.OBD("/dev/rfcomm0", baudrate=9600)
+    if connection.status() != obd.OBDStatus.NOT_CONNECTED:
+        break
+    connection.close()
+    time.sleep(1)
 
-response = connection.query(obd.commands.ELM_VOLTAGE)
-print(response.value)
-response = connection.query(obd.commands.ELM_VERSION)
-print(response.value)
+if connection.status() == obd.OBDStatus.NOT_CONNECTED:
+    sys.exit(1)
 
-if connection.status() == obd.OBDStatus.CAR_CONNECTED:
-    print("I think I can, I think I can...")
-    cmd = obd.commands.SPEED # select an OBD command (sensor)
+for x in range(0, 5):
+    response = connection.query(obd.commands.ELM_VOLTAGE)
+    print(response.value)
+    response = connection.query(obd.commands.ELM_VERSION)
+    print(response.value)
+    time.sleep(1)
 
-    response = connection.query(cmd) # send the command, and parse the response
+while True:
+    time.sleep(2)
+    if connection.status() == obd.OBDStatus.CAR_CONNECTED:
+        print("CAR_CONNECTED: I think I can, I think I can...")
+        cmd = obd.commands.SPEED # select an OBD command (sensor)
 
-    print(response.value) # returns unit-bearing values thanks to Pint
-    print(response.value.to("mph")) # user-friendly unit conversions
+        response = connection.query(cmd) # send the command, and parse the response
 
-    c = obd.commands.RPM
-    response = connection.query(c)
-    print(response.value) # returns unit-bearing values thanks to Pint
-    #print(response.value.to("rpm")) # user-friendly unit conversions
+        print(response.value) # returns unit-bearing values thanks to Pint
+        print(response.value.to("mph")) # user-friendly unit conversions
 
-if connection.status() == obd.OBDStatus.OBD_CONNECTED:
-    print("OBD_CONNECTED, ignition off")
-    print(connection.supported_commands)
+        c = obd.commands.RPM
+        response = connection.query(c)
+        print(response.value) # returns unit-bearing values thanks to Pint
+        #print(response.value.to("rpm")) # user-friendly unit conversions
+        print(connection.supported_commands)
+
+    if connection.status() == obd.OBDStatus.OBD_CONNECTED:
+        print("OBD_CONNECTED, ignition off")
+        response = connection.query(obd.commands.ELM_VOLTAGE)
+        print(response.value)
